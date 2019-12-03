6b312faea207ffcd7fc1d841566997acde953bb6
rajendrachavan <rajendrachavan.jvm@gmail.com>
2019-12-03 20:51:08 +0530

    Initial Commit(Local part2)

---
+++ src/test/java/neo/spring5/microservices/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package neo.spring5.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
